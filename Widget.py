dc190500adf301390741557679732eca08b73471
Arnaud-Baril <insommelierswetrust@gmail.com>
2026-03-13 10:31:59 -0400

    Update Widget.py

--- Widget.py
+++ Widget.py
@@ -1,15 +1,21 @@
 import tkinter as tk
 
 root = tk.Tk(baseName= 'Coming Week')
+
+root.attributes('-alpha', 0.8)
+
+#bg est pour background color
+root.configure(bg='red')
 # Widgets are added here
-label = tk.Label(root, text="Prochain 7 jours")
+label = tk.Label(root, text="Prochain 7 jours", bg='red')
 label.grid(row=0, column=0)
 var1 = tk.IntVar()
 var2 = tk.IntVar()
 
+
 """Sticky=tk.W fait que le widget(la coche et texte) colle l'endroit specifie apres le =. W veux dire West. la coche colle 
 a gauche de la fenetre. exemple, S en bas."""
-tk.Checkbutton(root, text="Fait", variable=var1).grid(row=1, sticky=tk.W)
+tk.Checkbutton(root, text="Fait", variable=var1, bg='red').grid(row=1, sticky=tk.W)
 
 
 
