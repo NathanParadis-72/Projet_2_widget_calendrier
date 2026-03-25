8e79ac29ba6ba40b6c701a1b53808b5824442678
Arnaud-Baril <insommelierswetrust@gmail.com>
2026-03-25 10:24:48 -0400

    Update Widget.py

--- Widget.py
+++ Widget.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+
 import tkinter as tk
 from tkinter import ttk
 from tkinter import *
@@ -8,10 +9,18 @@ root = tk.Tk(baseName= 'Coming Week')
 #obtient seulement la date d'aujourd'hui
 now = datetime.now().date()
 
-#a confirmer ce que cela fait!!!!
-root.attributes('-alpha', 0.8)
+#cree un nombre a partir de la date pour comparer
+now_compare = datetime.now().strftime("%y%m%d")
 
-Lundi_text = [[{"nom de l'evenement": 'evenement du lundi'}, {'temps de debut': '20260312T133000Z'}, {'temps de fin': '20260312T153000Z'}]]
+#donne la date des jours a venir avec des additions de journees
+day2 = now + timedelta(days=1)
+day3 = now + timedelta(days=2)
+day4 = now + timedelta(days=3)
+day5 = now + timedelta(days=4)
+day6 = now + timedelta(days=5)
+day7 = now + timedelta(days=6)
+
+liste_lundi = [{"nom": 'nom event', 'debut': '20260314T160000Z', 'temps de fin': '20260314T170000Z'}, {"nom de l'evenement": 'TESTTESTTEST', 'temps de debut': '20260312T133000Z', 'temps de fin': '20260312T153000Z'}]
 
 #necessaire a faire fonctionner GUI avec python
 var1 = tk.IntVar()
@@ -26,59 +35,59 @@ root.attributes("-transparentcolor", "red")
 root.configure(bg='red')
 
 # Label est du texte ecrit. 
-lundi = tk.Label(text=f" lundi {now}             ", bg='red', fg='white', font = 20)
+lundi = tk.Label(text=f" {now}             ", bg='red', fg='white', font = 20)
 lundi.grid(row=0, column=0)
-lundi_event = tk.Label(wraplength= 200,  anchor = W, justify= LEFT, text = Lundi_text, bg="red", fg='white', font = ("Arial", 10))
+lundi_event = tk.Label(wraplength= 200,  anchor = W, justify= LEFT, text = f"{liste_lundi[0]["nom"]}\n {liste_lundi[0]["debut"]}" , bg="red", fg='white', font = ("Arial", 10))
 lundi_event.grid(row=1, column=0)
 
 
-mardi = tk.Label(text = " mardi            ", bg='red', fg='white', font = 20)
+mardi = tk.Label(text = f" {day2}             ", bg='red', fg='white', font = 20)
 mardi.grid (row=0, column=1)
 
 
-mercredi = tk.Label(text = " mercredi         ", bg='red', fg='white', font = 20)
+mercredi = tk.Label(text = f" {day3}         ", bg='red', fg='white', font = 20)
 mercredi.grid (row=0, column=2)
 
 
-jeudi = tk.Label(text = " jeudi        ", bg='red', fg='white', font = 20)
+jeudi = tk.Label(text = f" {day4}        ", bg='red', fg='white', font = 20)
 jeudi.grid (row=0, column=3)
 
 
-vendredi = tk.Label(text = " vendredi          ", bg='red', fg='white', font = 20)
+vendredi = tk.Label(text = f" {day5}          ", bg='red', fg='white', font = 20)
 vendredi.grid (row=0, column=4)
 
 
-samedi = tk.Label(text = " samedi           ", bg='red', fg='white', font = 20)
+samedi = tk.Label(text = f" {day6}           ", bg='red', fg='white', font = 20)
 samedi.grid (row=0, column=5)
 
 
-dimanche = tk.Label(text = " dimanche         ", bg='red', fg='white', font = 20)
+dimanche = tk.Label(text = f" {day7}         ", bg='red', fg='white', font = 20)
 dimanche.grid (row=0, column=6)
 
 
-sepmardi = ttk.Separator(root, orient='vertical')
+sepmardi = ttk.Separator(orient='vertical')
 
-sepmardi.grid(row=0, column=1, columnspan=1,  rowspan=12, sticky="wns" , padx=0, pady=0)
+sepmardi.grid(row=0, column=1, columnspan=1,  rowspan=12, sticky="wns")
 
-sepmercredi = ttk.Separator(root, orient='vertical')
+sepmercredi = ttk.Separator(orient='vertical')
 
-sepmercredi.grid(row=0, column=2, columnspan=1,  rowspan=12, sticky="wns" , padx=0, pady=0)
+sepmercredi.grid(row=0, column=2, columnspan=1,  rowspan=12, sticky="wns")
 
-sepjeudi = ttk.Separator(root, orient='vertical')
+sepjeudi = ttk.Separator(orient='vertical')
 
-sepjeudi.grid(row=0, column=3, columnspan=1,  rowspan=12,sticky="wns" , padx=0, pady=0)
+sepjeudi.grid(row=0, column=3, columnspan=1,  rowspan=12,sticky="wns")
 
-sepvendredi = ttk.Separator(root, orient='vertical')
+sepvendredi = ttk.Separator(orient='vertical')
 
-sepvendredi.grid(row=0, column=4, columnspan=1,  rowspan=12, sticky="wns" , padx=0, pady=0)
+sepvendredi.grid(row=0, column=4, columnspan=1,  rowspan=12, sticky="wns")
 
-sepsamedi = ttk.Separator(root, orient='vertical')
+sepsamedi = ttk.Separator(orient='vertical')
 
-sepsamedi.grid(row=0, column=5, columnspan=1,  rowspan=12, sticky="wns" , padx=0, pady=0)
+sepsamedi.grid(row=0, column=5, columnspan=1,  rowspan=12, sticky="wns")
 
-sepdimanche = ttk.Separator(root, orient='vertical')
+sepdimanche = ttk.Separator(orient='vertical')
 
-sepdimanche.grid(row=0, column=6, columnspan=1,  rowspan=12, sticky="wns" , padx=0, pady=0)
+sepdimanche.grid(row=0, column=6, columnspan=1,  rowspan=12, sticky="wns")
 
 
 """Sticky=tk.W fait que le widget(la coche et texte) colle l'endroit specifie apres le =. W veux dire West. la coche colle 
@@ -88,3 +97,4 @@ tk.Checkbutton(root, text="Fait", variable=var1, bg='red', fg='white').grid(row=
 
 
 root.mainloop()
+
