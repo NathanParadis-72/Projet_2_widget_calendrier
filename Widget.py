34b854eef9037a4eb109384b68269a5f99c9f7dc
Arnaud-Baril <insommelierswetrust@gmail.com>
2026-03-28 17:41:20 -0400

    Update Widget.py

--- Widget.py
+++ Widget.py
@@ -13,27 +13,33 @@ root.attributes("-transparentcolor", "red")
 jour1 = datetime.now().date()
 
 #la variable contenant le calendrier.
-ical = [{'nom': "Nom de l'evenement", 'debut': ['20260328', '160000z'], 'fin': ['20260314', '170000Z']}, {'nom': 'evenement 2', 'debut': ['20260329', '133000Z'], 'fin': ['20260312', '153000Z']}]
+calendrier = [{'nom': "Nom de l'evenement", 'debut': ['20260328', '160000z'], 'fin': ['20260314', '170000Z']}, {'nom': 'evenement 2', 'debut': ['20260329', '133000Z'], 'fin': ['20260312', '153000Z']}]
 
-def affichage(day,jour,colonne):
-    daycal = tk.Label(text=f"       {jour}      ", bg = 'red', fg ='white', font = 20)
-    daycal.grid(row = 0, column = colonne)
-    now_compare = jour.strftime("%Y%m%d")
+def affichage(evenements,date,colonne):
     horaire = ""
-    for event in range(len(ical)):
-        if ical[event]['debut'][0] == now_compare:
-            day.append(ical[event])
-    for event in range(len(day)):
-       horaire += f"{day[event]['nom']}\n start:{day[event]['debut'][1]}\n end:{day[event]['fin'][1]}\n\n "
-    day_event = tk.Label(wraplength= 400,  anchor = tk.W, justify= tk.LEFT, text = f" {horaire}", bg="red", fg='white', font = ("Arial", 10))
-    day_event.grid(row = 1, column = colonne)
+    date_du_jour = tk.Label(text=f"       {date}      ", bg = 'red', fg ='white', font = 20)
+    date_du_jour.grid(row = 0, column = colonne)
+    now_compare = date.strftime("%Y%m%d") 
+
+    for event in range(len(calendrier)):
+        if calendrier[event]['debut'][0] == now_compare:
+            evenements.append(calendrier[event])
+
+    for event in range(len(evenements)):
+       horaire += f"{evenements[event]['nom']}\n start:{evenements[event]['debut'][1]}\n end:{evenements[event]['fin'][1]}\n\n "
+
+    evenement_du_jour = tk.Label(wraplength= 400,  anchor = tk.W, justify= tk.LEFT, text = f" {horaire}", bg="red", fg='white', font = ("Arial", 10))
+    evenement_du_jour.grid(row = 1, column = colonne)
+
     separateur = ttk.Separator(orient='vertical')
     separateur.grid(row=0, column=colonne, columnspan=1,  rowspan=12, sticky="wns")
 
+
+
 for i in range(7):
-    agenda = jour1 + timedelta(days = i)
-    day = []
-    affichage(day, agenda, i)
+    date = jour1 + timedelta(days = i)
+    evenements = []
+    affichage(evenements, date, i)
 
 root.mainloop()
 
